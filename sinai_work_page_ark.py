66504ad1e9e0b60cbb5f3b10b0fce73dddc462f3
Ashton Prigge <ashton.prigge@gmail.com>
2020-03-26 09:45:49 -0700

    LEG-180: Added function to strip carraige returns.

--- sinai_work_page_ark.py
+++ sinai_work_page_ark.py
@@ -3,6 +3,13 @@ import os
 import subprocess
 import pandas as pd
 
+
+def clean(dirty):
+    '''
+    Returns a cleaned string, stripped of all newlines and carriage returns.
+    '''
+    return dirty.replace('\r', '').replace('\n', '')
+
 #creates a noid format file that suplies the parent ark for the NOID to be appended to
 
 def create_noid_yml(parent_ark):
@@ -38,14 +45,15 @@ for row in works_cursor:
         create_mappings()
         cmd_ezid = ['python', 'ezid.py', ezid_input, 'mint', ark_shoulder, '@', 'mappings.txt']
         parent_ark = subprocess.Popen(cmd_ezid, stdout=subprocess.PIPE).communicate()[0]
-        parent_ark = (str(parent_ark)).replace('success: ', '')
+        parent_ark = clean(str(parent_ark)).replace('success: ', '')
         parent_ark_list.append(parent_ark)
         ark_dict[altidentifer] = parent_ark
+    if row['Object Type'] == '':
+    	parent_ark_list.append('')
     elif row['Item ARK'] != '':
         parent_ark = row['Item ARK']
         parent_ark_list.append(parent_ark)
         ark_dict[altidentifer] = parent_ark
-
 data= pd.read_csv(works_file, sep=',', delimiter=None, header='infer')
 data = data.drop("Item ARK", axis=1)
 data.insert(7, 'Item ARK', parent_ark_list)
@@ -54,6 +62,7 @@ data.to_csv(path_or_buf=(directory+output_file), sep=',', na_rep='', float_forma
 #loops through files in a directory 
 check_ark_list = []
 for filename in os.listdir(directory):
+    print(filename)
     if '.csv' in filename and filename != 'works.csv':
         file_path = directory + (str(filename))
         cursor = csv.DictReader(open(file_path),
@@ -66,10 +75,11 @@ for filename in os.listdir(directory):
             if row['Object Type'] == 'ChildWork':
                 source = row['Source']
                 if source in ark_dict.keys():
-                    parent_ark = ark_dict[source]
+                    parent_ark = clean(ark_dict[source])
                     create_noid_yml(parent_ark)
                     cmd = ['noid', '-f', 'Noid_test.yml']
                     item_ark = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+                    item_ark = clean(item_ark)
                     item_ark_list.append(item_ark)
                     local_parent_ark_list.append(parent_ark)
                     check_ark_list.append(parent_ark_list)
